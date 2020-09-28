a97ec364f1a0eb28532d52fe00c7f1938f74f074
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-28 20:27:41 +0800

    >  compile NEMU 3019244360 gaoshutao Linux ubuntu 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:27:41 up  2:06,  1 user,  load average: 0.33, 0.24, 0.12 3ad20a794257f29fc388438e8277ac3326026f44

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,23 @@
 typedef struct watchpoint {
 	int NO;
 	struct watchpoint *next;
-
+	char expr[64];
+	uint32_t ans;
 	/* TODO: Add more members if necessary */
 
 
 } WP;
+
+WP* new_wp();
+
+void wp_free(WP*wp);
+
+WP* getHead();
+
+int insertExpr(char*ex);
+
+int removeNode(int id);
+
+int checkNode(WP*nd);
 
 #endif
