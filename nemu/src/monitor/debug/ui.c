6cac3f545ce9bfb16b97257c628ae1e2ea0687ea
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 15:46:54 +0800

    >  compile NEMU 3019244360 gaoshutao Linux ubuntu 5.3.0-59-generic #53~18.04.1-Ubuntu SMP Thu Jun 4 14:58:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:46:54 up  6:57,  1 user,  load average: 0.34, 0.16, 0.10 1af29ddd25fff0522806ae6ff5bca09de40e8bc3

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -74,6 +74,12 @@ static int cmd_x(char*args){
 		printf("lack of options ,please input some .\n");
 		return 0; 
 	}
+	uint32_t num, addr;
+	sscanf(args, "%d%x", &num, &addr);
+	while(num--) {
+		printf("address 0x%x: 0x%x\n", addr, swaddr_read(addr, 4));
+		addr += 4;
+	}
 	return 0;
 }
 
@@ -81,12 +87,6 @@ static int cmd_p(char*args){
 	if(args == NULL) {
 		printf("lack of options ,please input some .\n");
 		return 0; 
-	}
-	uint32_t num, addr;
-	sscanf(args, "%d%x", &num, &addr);
-	while(num--) {
-		printf("address 0x%x: 0x%x\n", addr, swaddr_read(addr, 4));
-		addr += 4;
 	}
 	return 0;
 }
